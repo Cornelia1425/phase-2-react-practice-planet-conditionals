0f9fe38e60424d0e6a9a568c998066e4a451523d
yiranshu <shuyiran1425@hotmail.com>
2024-04-30 14:55:15 -0400

    planet

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,37 @@ import StarField from '../visual-effects/StarField'
 import PlanetsContainer from "./PlanetsContainer"
 
 function App() {
+  
+  const [planets, setPlanets] = useState([])
+  const [isDark, setIsDark] = useState(true)
 
-  const [planets, setPlanets] = useState('')
+  const handleDark = ()=>{
+    setIsDark(!isDark)
+  }
+
 
   useEffect(() => {
     console.log('fetching planets...')
     fetch('http://localhost:4001/planets')
     .then(res => res.json())
-    .then(data => setPlanets(data))
-  })
+    .then(planetsArray => setPlanets(planetsArray))
+  }, [])
+  console.log("planets: ",planets)
 
   return (
-    <div className="App">
-
+    <div className={"App " + (isDark? "dark-mode" :"light-mode")} >
+      
       {/* You won't have to touch StarField, it's just here for visual things */}
       <StarField />
 
-      <button>Light Mode</button>
+      <button onClick={handleDark}> {isDark? "Dark Mode":"Light Mode"}</button>
 
       <PlanetsContainer planets={planets} />
+      
 
     </div>
   );
 }
 
 export default App;
+
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import PlanetFacts from "./PlanetFacts"
+import {useState} from 'react'
 
 function PlanetCard({planet}) {
+  const [isImage, setIsImage] = useState(true)
+ /*  const handleFlip =()=>{
+    setIsImage(!isImage)
+  } */
 
   return (
-    <div className="card" onClick={() => console.log('Toggle!')}>
+    <div className="card" onClick={()=>setIsImage(!isImage)}> {/* () => console.log('Toggle!') */}
+    {isImage?  
+      (<>
       <h2>{planet.name}</h2>
-      <img className="" src={planet.image} alt={planet.name} />
+      <img className={planet.is_planet ? "" : "not-planet"} src={planet.image} alt={planet.name} />
+      </>):
+      (<PlanetFacts planet={planet}/>)
+      }
     </div>
   )
 }
